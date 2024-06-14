7c448619b152a9fa914d74f497bd96b7ac6b2caf
Cristian Morales Hammouche <cristian96.cm@gmail.com>
2024-06-14 17:58:12 +0000

    fct games con ids

--- models/marts/core/fct_games.sql
+++ models/marts/core/fct_games.sql
@@ -8,7 +8,18 @@ source as (
 renamed as (
 
     select
-        *
+        game_id,
+        date_id,
+        team_id_home,
+        team_id_away,
+        home_score,
+        away_score,
+        tournament_id,
+        country_id,
+        city_id,
+        score_id,
+        match,
+        result
     from source 
 
 )
