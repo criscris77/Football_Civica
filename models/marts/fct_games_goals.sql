76b28a2deb554d7913b51b2e937fc258525a36f4
Cristian Morales Hammouche <cristian96.cm@gmail.com>
2024-06-16 13:05:51 +0000

    incremental

--- models/marts/fct_games_goals.sql
+++ models/marts/fct_games_goals.sql
@@ -1,6 +1,9 @@
-with 
+{{ config(
+    materialized='incremental',
+    unique_key='score_id'
+) }}
 
-source as (
+with source as (
     select * from {{ ref('intermediate_football_games') }}
 ),
 
@@ -9,19 +12,22 @@ renamed as (
         a.*
     from source a 
     inner join {{ ref('dim_tournament') }} b 
-    on a.tournament=b.tournament
-    inner join  {{ ref('dim_country') }} c 
-    on a.country=c.country
-    inner join  {{ ref('dim_city') }} d 
-    on a.city=d.city
+        on a.tournament = b.tournament
+    inner join {{ ref('dim_country') }} c 
+        on a.country = c.country
+    inner join {{ ref('dim_city') }} d 
+        on a.city = d.city
     inner join {{ ref('dim_time') }} e
-    on a.date=e.date
+        on a.date = e.date
     inner join {{ ref('dim_teams') }} f
-    on a.home_team=f.team
+        on a.home_team = f.team
     inner join {{ ref('dim_teams') }} g
-    on a.away_team=g.team
-    
+        on a.away_team = g.team
 )
 
 select * from renamed
+{% if is_incremental() %}
+    WHERE _fivetran_synced > (SELECT COALESCE(MAX(_fivetran_synced), '1900-01-01') FROM {{ this }} )
+{% endif %}
 ORDER BY date DESC
+
